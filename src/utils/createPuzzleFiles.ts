3b36fbd0c175abb5051e82bbbd45e429131112a5
LindaKat <lindakt16@gmail.com>
2025-12-01 15:32:20 -0600

    fixed link linting in readme; updated default year for new puzzle files

--- src/utils/createPuzzleFiles.ts
+++ src/utils/createPuzzleFiles.ts
@@ -1,6 +1,6 @@
 import { mkdir, writeFile } from "fs/promises";
 
-const defaultYear = 2024;
+const defaultYear = 2025;
 const puzzleDay = Number(process.env.npm_config_date);
 const puzzleYear = Number(`20${process.env.npm_config_year}`) || defaultYear;
 
